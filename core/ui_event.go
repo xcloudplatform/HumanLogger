38706f15f9d0ca0ac73233bc7b0d35b290399301
Standard Template <standardtemplateconstruct2022@gmail.com>
2023-05-12 15:44:01 +0300

    changes

--- core/ui_event.go
+++ core/ui_event.go
@@ -1,21 +1,20 @@
 package core
 
 import (
+	"encoding/json"
 	"fmt"
 	hook "github.com/robotn/gohook"
+	"strconv"
 	"time"
 )
 
 type UIEvent struct {
-	Timestamp time.Time `json:"timestamp"`
-
 	hook.Event
 }
 
 func NewUIEvent(event hook.Event) UIEvent {
 	return UIEvent{
-		Timestamp: time.Now(),
-		Event:     event,
+		Event: event,
 	}
 }
 
@@ -63,4 +62,96 @@ func (e UIEvent) String() string {
 	}
 
 	return "Unknown event, contact the mantainers."
+}
+
+func (e UIEvent) MarshalJSON() ([]byte, error) {
+	type Alias UIEvent
+
+	switch e.Kind {
+	case hook.KeyUp, hook.KeyHold, hook.KeyDown:
+		// If it's a Keyboard event the relevant fields are:
+		// Mask, Keycode, Rawcode, and Keychar,
+
+		return json.Marshal(&struct {
+			When    int64  `json:"timestamp"`
+			Kind    string `json:"kind"`
+			Mask    uint16 `json:"mask"`
+			Keycode uint16 `json:"keycode"`
+			Rawcode uint16 `json:"rawcode"`
+			//Keychar rune   `json:"keychar"`
+		}{
+			When:    e.When.UnixNano() / int64(time.Millisecond),
+			Kind:    e.kindToStr(),
+			Mask:    e.Mask,
+			Keycode: e.Keycode,
+			Rawcode: e.Rawcode,
+			//Keychar: e.Keychar,
+		})
+
+	case hook.MouseHold, hook.MouseUp, hook.MouseMove, hook.MouseDrag, hook.MouseDown, hook.MouseWheel:
+		// If it's a Mouse event the relevant fields are:
+		// Button, Clicks, X, Y, Amount, Rotation and Direction
+
+		return json.Marshal(&struct {
+			When      int64  `json:"timestamp"`
+			Kind      string `json:"kind"`
+			Button    uint16 `json:"button"`
+			Clicks    uint16 `json:"clicks"`
+			X         int16  `json:"x"`
+			Y         int16  `json:"y"`
+			Amount    uint16 `json:"amount"`
+			Rotation  int32  `json:"rotation"`
+			Direction uint8  `json:"direction"`
+		}{
+			When:      e.When.UnixNano() / int64(time.Millisecond),
+			Kind:      e.kindToStr(),
+			Button:    e.Button,
+			Clicks:    e.Clicks,
+			X:         e.X,
+			Y:         e.Y,
+			Amount:    e.Amount,
+			Rotation:  e.Rotation,
+			Direction: e.Direction,
+		})
+	default:
+		return json.Marshal(&struct {
+			When int64  `json:"timestamp"`
+			Kind string `json:"kind"`
+		}{
+			When: e.When.UnixNano() / int64(time.Millisecond),
+			Kind: e.kindToStr(),
+		})
+
+	}
+}
+
+func (e UIEvent) kindToStr() string {
+	switch e.Kind {
+	case hook.HookEnabled:
+		return "HookEnabled"
+	case hook.HookDisabled:
+		return "HookDisabled"
+	case hook.KeyDown:
+		return "KeyDown"
+	case hook.KeyHold:
+		return "KeyHold"
+	case hook.KeyUp:
+		return "KeyUp"
+	case hook.MouseUp:
+		return "MouseUp"
+	case hook.MouseHold:
+		return "MouseHold"
+	case hook.MouseDown:
+		return "MouseDown"
+	case hook.MouseMove:
+		return "MouseMove"
+	case hook.MouseDrag:
+		return "MouseDrag"
+	case hook.MouseWheel:
+		return "MouseWheel"
+	case hook.FakeEvent:
+		return "FakeEvent"
+	default:
+		return "Unknown Kind " + strconv.Itoa(int(e.Kind))
+	}
 }
